832bc576830e518810b65b2b868831bb786bbfcd
Trys <trysmudford@gmail.com>
2019-02-16 07:58:21 +0000

    Stronger PW

--- src/js/pw.js
+++ src/js/pw.js
@@ -1,30 +1,40 @@
 (function() {
-  const wrap = document.querySelector('.password')
-  if (!wrap) return
+  const wrap = document.querySelector('.password');
+  if (!wrap) return;
 
-  const input = 'ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz?!*^%0123456789'.split('')
-  const output = wrap.querySelector('.password__result')
+  const input = 'ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz?!*^%£+_-=$%&{}0123456789'.split(
+    ''
+  );
+  const output = wrap.querySelector('.password__result');
 
-  const rand = (min, max) => Math.floor(Math.random() * ( max - min + 1) + min)
-  const generate = () => output.innerText = Array.from(Array(15)).map(x => input[rand(0, input.length - 1)]).join('')
-  generate()
-  wrap.querySelector('.password__generate').addEventListener('click', generate)
+  const rand = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+  const generate = () =>
+    (output.innerText = Array.from(Array(30))
+      .map(x => input[rand(0, input.length - 1)])
+      .join(''));
 
-  wrap.querySelector('.password__copy')  
-    .addEventListener('click', (event) => {
-      const range = document.createRange();
-      range.selectNode(output);  
-      window.getSelection().addRange(range);  
+  generate();
+  wrap.querySelector('.password__generate').addEventListener('click', generate);
 
-      try {
-        document.execCommand('copy');
-        event.target.classList.add('button--success')
-      } catch(e) {
-        event.target.classList.add('button--failure')
-      }
+  wrap.querySelector('.password__copy').addEventListener('click', event => {
+    const range = document.createRange();
+    range.selectNode(output);
+    window.getSelection().addRange(range);
 
-      setTimeout(() => ['button--failure', 'button--success'].forEach(c => event.target.classList.remove(c)), 2000) 
-      window.getSelection().removeAllRanges();  
-    })
+    try {
+      document.execCommand('copy');
+      event.target.classList.add('button--success');
+    } catch (e) {
+      event.target.classList.add('button--failure');
+    }
 
-})()
+    setTimeout(
+      () =>
+        ['button--failure', 'button--success'].forEach(c =>
+          event.target.classList.remove(c)
+        ),
+      2000
+    );
+    window.getSelection().removeAllRanges();
+  });
+})();
